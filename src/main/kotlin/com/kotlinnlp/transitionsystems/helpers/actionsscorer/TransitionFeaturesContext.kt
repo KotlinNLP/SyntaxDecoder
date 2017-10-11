f309cb14f89403698a1d4a05ba38394e9ef92d63
KotlinNLP Authors <github@kotlinnlp.com>
2017-10-11 16:37:11 +0200

    Add features contexts

---
+++ src/main/kotlin/com/kotlinnlp/transitionsystems/helpers/actionsscorer/TransitionFeaturesContext.kt
@@ -0,0 +1,20 @@
+/* Copyright 2017-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package com.kotlinnlp.transitionsystems.helpers.actionsscorer
+
+import com.kotlinnlp.transitionsystems.State
+import com.kotlinnlp.transitionsystems.Transition
+
+/**
+ * @property state a [State]
+ * @property transition a [Transition]
+ */
+data class TransitionFeaturesContext<StateType: State<StateType>, TransitionType: Transition<TransitionType, StateType>>(
+  val state: StateType,
+  val transition: Transition<TransitionType, StateType>
+) : FeaturesContext<TransitionFeaturesContext<StateType, TransitionType>>

8574210c09ab9779624147db5147c4358021575b
KotlinNLP Authors <github@kotlinnlp.com>
2017-10-11 16:38:20 +0200

    Add ActionsScorer interface

---
+++ src/main/kotlin/com/kotlinnlp/transitionsystems/helpers/actionsscorer/ActionsScorer.kt
@@ -0,0 +1,39 @@
+/* Copyright 2017-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package com.kotlinnlp.transitionsystems.helpers.actionsscorer
+
+import com.kotlinnlp.transitionsystems.State
+import com.kotlinnlp.transitionsystems.Transition
+
+/**
+ * The ActionsScorer.
+ *
+ * @property featuresExtractor a [FeaturesExtractor]
+ */
+abstract class ActionsScorer<
+  StateType: State<StateType>,
+  TransitionType: Transition<TransitionType, StateType>,
+  FeaturesType: Features<FeaturesType>,
+  FeaturesContextType: FeaturesContext<FeaturesContextType>>(
+  val featuresExtractor: FeaturesExtractor<FeaturesType, FeaturesContextType>
+) {
+
+  /**
+   * Assign a score to each action of the given list.
+   *
+   * @param actions a list of actions to score
+   */
+  abstract fun score(actions: List<Transition<TransitionType, StateType>.Action>)
+
+  /**
+   * @return a map of Transitions to their related Actions
+   */
+  protected fun List<Transition<TransitionType, StateType>.Action>.mapToTransitions():
+    Map<Transition<TransitionType, StateType>, List<Transition<TransitionType, StateType>.Action>>
+    = this.groupBy { it.transition }
+}

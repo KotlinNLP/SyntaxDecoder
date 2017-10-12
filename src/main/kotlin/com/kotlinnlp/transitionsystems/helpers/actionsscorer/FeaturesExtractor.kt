1463ee61c6b6520c3a7e8e43c56adbace86cc67c
KotlinNLP Authors <github@kotlinnlp.com>
2017-10-12 17:41:11 +0200

    Update extract() method arguments adding itemsContext

--- src/main/kotlin/com/kotlinnlp/transitionsystems/helpers/actionsscorer/FeaturesExtractor.kt
+++ src/main/kotlin/com/kotlinnlp/transitionsystems/helpers/actionsscorer/FeaturesExtractor.kt
@@ -9,6 +9,7 @@ package com.kotlinnlp.transitionsystems.helpers.actionsscorer
 
 import com.kotlinnlp.transitionsystems.helpers.actionsscorer.features.Features
 import com.kotlinnlp.transitionsystems.helpers.actionsscorer.stateview.StateView
+import com.kotlinnlp.transitionsystems.state.ItemsContext
 
 /**
  * The FeaturesExtractor.
@@ -16,7 +17,12 @@ import com.kotlinnlp.transitionsystems.helpers.actionsscorer.stateview.StateView
 interface FeaturesExtractor<in StateViewType : StateView, out FeaturesType : Features<*, *>> {
 
   /**
+   * Extract features using the given [stateView] and an optional [itemsContext].
    *
+   * @param stateView a view of the state
+   * @param itemsContext items context (optional)
+   *
+   * @return the extracted [Features]
    */
-  fun extract(stateView: StateViewType): FeaturesType
+  fun extract(stateView: StateViewType, itemsContext: ItemsContext<*>?): FeaturesType
 }

be55c4cc7c253f6b2c238cca3433804b271ad8b9
KotlinNLP Authors <github@kotlinnlp.com>
2017-10-12 17:38:40 +0200

    Add ExtendedState

---
+++ src/main/kotlin/com/kotlinnlp/transitionsystems/state/ExtendedState.kt
@@ -0,0 +1,38 @@
+/* Copyright 2017-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package com.kotlinnlp.transitionsystems.state
+
+import com.kotlinnlp.transitionsystems.utils.Clonable
+
+/**
+ * The [ExtendedState] extends a [State] with the list of [StateItem]s that compose it and a [ItemsContext].
+ */
+data class ExtendedState<
+  SelfType: ExtendedState<SelfType, StateType, ItemErrorsType, ItemRelevanceType, ItemType, ContextType>,
+  StateType: State<StateType>,
+  ItemErrorsType: ItemErrors<ItemErrorsType>,
+  ItemRelevanceType: ItemRelevance<ItemRelevanceType>,
+  ItemType: StateItem<ItemType, ItemErrorsType, ItemRelevanceType>,
+  ContextType: ItemsContext<ContextType>>(
+  val state: StateType,
+  val items: List<ItemType>,
+  val context: ContextType?
+) : Clonable<SelfType> {
+
+  /**
+   * @return a copy of this [ExtendedState]
+   */
+  override fun copy(): SelfType {
+
+    @Suppress("UNCHECKED_CAST")
+    return ExtendedState<SelfType, StateType, ItemErrorsType, ItemRelevanceType, ItemType, ContextType>(
+      state = this.state.copy(),
+      items = this.items.map { it.copy() },
+      context = this.context?.copy()) as SelfType
+  }
+}

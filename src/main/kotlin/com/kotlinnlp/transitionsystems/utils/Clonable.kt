93753cbe7e3eff283e8ecf5db0d956838090ea21
KotlinNLP Authors <github@kotlinnlp.com>
2017-10-12 17:37:22 +0200

    Add Clonable interface

---
+++ src/main/kotlin/com/kotlinnlp/transitionsystems/utils/Clonable.kt
@@ -0,0 +1,20 @@
+/* Copyright 2017-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package com.kotlinnlp.transitionsystems.utils
+
+/**
+ * Defines an object that has the method copy(), which returns a new object of the same type, containing a copy of its
+ * content.
+ */
+interface Clonable<out SelfType> {
+
+  /**
+   * @return a copy of this object
+   */
+  fun copy(): SelfType
+}

df75e58e5ff58c91db6e4025200d2a1e6c52b6b5
KotlinNLP Authors <github@kotlinnlp.com>
2017-10-26 14:15:23 +0200

    Replace stateView with state and transition

--- src/main/kotlin/com/kotlinnlp/transitionsystems/models/arcspine/ArcSpineVirtualState.kt
+++ src/main/kotlin/com/kotlinnlp/transitionsystems/models/arcspine/ArcSpineVirtualState.kt
@@ -7,18 +7,16 @@
 
 package com.kotlinnlp.transitionsystems.models.arcspine
 
-import com.kotlinnlp.transitionsystems.state.stateview.StateTransitionView
 import com.kotlinnlp.transitionsystems.models.arcspine.transitions.*
 
 /**
  * The ArcSpineVirtualState is a higher level of abstraction of a [ArcSpineState] providing a uniform view of the state
- * that might varies for each transition.
+ * in relation with a specific transition.
  *
- * @param stateView a transition-based state view
+ * @param state an ArcSpine state
+ * @param transition an ArcSpine transition
  */
-class ArcSpineVirtualState(
-  private val stateView: StateTransitionView<ArcSpineState, ArcSpineTransition>
-) {
+class ArcSpineVirtualState(private val state: ArcSpineState, private val transition: ArcSpineTransition) {
 
   /**
    * The virtual stack.
@@ -46,7 +44,7 @@ class ArcSpineVirtualState(
    * Initialize the virtual stack and buffer respect to the current 'stateView'.
    */
   init {
-    when (this.stateView.transition) {
+    when (this.transition) {
       is Shift -> this.extractShiftView()
       is ArcLeft -> this.extractArcLeftView()
       is ArcRight -> this.extractArcRightView()
@@ -59,15 +57,15 @@ class ArcSpineVirtualState(
    */
   private fun extractShiftView() {
 
-    val stackLastIndex: Int =  this.stateView.state.stack.lastIndex
+    val stackLastIndex: Int =  this.state.stack.lastIndex
 
-    this.stack[0] = this.stateView.state.stack.getTokenLeftSpine(stackLastIndex, 0)
-    this.stack[1] = this.stateView.state.stack.getTokenLeftSpine(stackLastIndex - 1, 0)
-    this.stack[2] = this.stateView.state.stack.getTokenLeftSpine(stackLastIndex - 2, 0)
-    this.stack[3] = this.stateView.state.stack.getTokenLeftSpine(stackLastIndex - 3, 0)
+    this.stack[0] = this.state.stack.getTokenLeftSpine(stackLastIndex, 0)
+    this.stack[1] = this.state.stack.getTokenLeftSpine(stackLastIndex - 1, 0)
+    this.stack[2] = this.state.stack.getTokenLeftSpine(stackLastIndex - 2, 0)
+    this.stack[3] = this.state.stack.getTokenLeftSpine(stackLastIndex - 3, 0)
 
-    this.buffer[0] = this.stateView.state.buffer.getOrNull(0)
-    this.buffer[1] = this.stateView.state.buffer.getOrNull(1)
+    this.buffer[0] = this.state.buffer.getOrNull(0)
+    this.buffer[1] = this.state.buffer.getOrNull(1)
 
     this.grandparent = null
     this.greatGrandparent = null
@@ -78,37 +76,37 @@ class ArcSpineVirtualState(
    */
   private fun extractArcLeftView() {
 
-    val stackLastIndex: Int = this.stateView.state.stack.lastIndex
+    val stackLastIndex: Int = this.state.stack.lastIndex
 
-    val k = (this.stateView.transition as ArcLeft).governorSpineIndex
+    val k = (this.transition as ArcLeft).governorSpineIndex
 
     this.grandparent = null
     this.greatGrandparent = null
 
-    this.stack[0] = this.stateView.state.stack.getTokenLeftSpine(index = stackLastIndex, k = k)
-    this.stack[1] = this.stateView.state.stack.getTokenLeftSpine(index = stackLastIndex - 1, k = 0)
-    this.stack[2] = this.stateView.state.stack.getTokenLeftSpine(index = stackLastIndex - 2, k = 0)
-    this.stack[3] = this.stateView.state.stack.getTokenLeftSpine(index = stackLastIndex - 3, k = 0)
+    this.stack[0] = this.state.stack.getTokenLeftSpine(index = stackLastIndex, k = k)
+    this.stack[1] = this.state.stack.getTokenLeftSpine(index = stackLastIndex - 1, k = 0)
+    this.stack[2] = this.state.stack.getTokenLeftSpine(index = stackLastIndex - 2, k = 0)
+    this.stack[3] = this.state.stack.getTokenLeftSpine(index = stackLastIndex - 3, k = 0)
 
     when (k) {
       0 -> {
-        this.buffer[0] = this.stateView.state.buffer.getOrNull(0)
-        this.buffer[1] = this.stateView.state.buffer.getOrNull(1)
+        this.buffer[0] = this.state.buffer.getOrNull(0)
+        this.buffer[1] = this.state.buffer.getOrNull(1)
       }
 
       1 -> {
-        this.buffer[0] = this.stateView.state.stack.getTokenLeftSpine(index = stackLastIndex, k = k - 1)
-        this.buffer[1] = this.stateView.state.buffer.getOrNull(0)
+        this.buffer[0] = this.state.stack.getTokenLeftSpine(index = stackLastIndex, k = k - 1)
+        this.buffer[1] = this.state.buffer.getOrNull(0)
 
-        this.grandparent = this.stateView.state.stack.getTokenLeftSpine(index = stackLastIndex, k = k - 1)
+        this.grandparent = this.state.stack.getTokenLeftSpine(index = stackLastIndex, k = k - 1)
       }
 
       else -> {
-        this.buffer[0] = this.stateView.state.stack.getTokenLeftSpine(index = stackLastIndex, k = k - 1)
-        this.buffer[1] = this.stateView.state.stack.getTokenLeftSpine(index = stackLastIndex, k = k - 2)
+        this.buffer[0] = this.state.stack.getTokenLeftSpine(index = stackLastIndex, k = k - 1)
+        this.buffer[1] = this.state.stack.getTokenLeftSpine(index = stackLastIndex, k = k - 2)
 
-        this.grandparent = this.stateView.state.stack.getTokenLeftSpine(index = stackLastIndex, k = k - 1)
-        this.greatGrandparent = this.stateView.state.stack.getTokenLeftSpine(index = stackLastIndex, k = k - 2)
+        this.grandparent = this.state.stack.getTokenLeftSpine(index = stackLastIndex, k = k - 1)
+        this.greatGrandparent = this.state.stack.getTokenLeftSpine(index = stackLastIndex, k = k - 2)
       }
     }
   }
@@ -118,47 +116,47 @@ class ArcSpineVirtualState(
    */
   private fun extractArcRightView() {
 
-    val stackLastIndex: Int = this.stateView.state.stack.lastIndex
+    val stackLastIndex: Int = this.state.stack.lastIndex
 
-    val k = (this.stateView.transition as ArcRight).governorSpineIndex
+    val k = (this.transition as ArcRight).governorSpineIndex
 
     this.grandparent = null
     this.greatGrandparent = null
 
-    this.stack[0] = this.stateView.state.stack.getTokenLeftSpine(index = stackLastIndex, k = 0)
-    this.stack[1] = this.stateView.state.stack.getTokenRightSpine(index = stackLastIndex - 1, k = k)
+    this.stack[0] = this.state.stack.getTokenLeftSpine(index = stackLastIndex, k = 0)
+    this.stack[1] = this.state.stack.getTokenRightSpine(index = stackLastIndex - 1, k = k)
 
     when (k) {
       0 -> {
-        this.stack[2] = this.stateView.state.stack.getTokenLeftSpine(index = stackLastIndex - 2, k = 0)
-        this.stack[3] = this.stateView.state.stack.getTokenLeftSpine(index = stackLastIndex - 3, k = 0)
+        this.stack[2] = this.state.stack.getTokenLeftSpine(index = stackLastIndex - 2, k = 0)
+        this.stack[3] = this.state.stack.getTokenLeftSpine(index = stackLastIndex - 3, k = 0)
       }
 
       1 -> {
-        this.stack[2] = this.stateView.state.stack.getTokenRightSpine(index = stackLastIndex - 1, k = k - 1)
-        this.stack[3] = this.stateView.state.stack.getTokenLeftSpine(index = stackLastIndex - 2, k = 0)
+        this.stack[2] = this.state.stack.getTokenRightSpine(index = stackLastIndex - 1, k = k - 1)
+        this.stack[3] = this.state.stack.getTokenLeftSpine(index = stackLastIndex - 2, k = 0)
 
-        this.grandparent = this.stateView.state.stack.getTokenRightSpine(index = stackLastIndex - 1, k = k - 1)
+        this.grandparent = this.state.stack.getTokenRightSpine(index = stackLastIndex - 1, k = k - 1)
       }
 
       else -> {
-        this.stack[2] = this.stateView.state.stack.getTokenRightSpine(index = stackLastIndex - 1, k = k - 1)
-        this.stack[3] = this.stateView.state.stack.getTokenRightSpine(index = stackLastIndex - 1, k = k - 2)
+        this.stack[2] = this.state.stack.getTokenRightSpine(index = stackLastIndex - 1, k = k - 1)
+        this.stack[3] = this.state.stack.getTokenRightSpine(index = stackLastIndex - 1, k = k - 2)
 
-        this.grandparent = this.stateView.state.stack.getTokenRightSpine(index = stackLastIndex - 1, k = k - 1)
-        this.greatGrandparent = this.stateView.state.stack.getTokenRightSpine(index = stackLastIndex - 1, k = k - 2)
+        this.grandparent = this.state.stack.getTokenRightSpine(index = stackLastIndex - 1, k = k - 1)
+        this.greatGrandparent = this.state.stack.getTokenRightSpine(index = stackLastIndex - 1, k = k - 2)
       }
     }
 
-    this.buffer[0] = this.stateView.state.buffer.getOrNull(0)
-    this.buffer[1] = this.stateView.state.buffer.getOrNull(1)
+    this.buffer[0] = this.state.buffer.getOrNull(0)
+    this.buffer[1] = this.state.buffer.getOrNull(1)
   }
 
   /**
    * Fill the virtual stack and buffer with respect to the Root transition.
    */
   private fun extractRootView() {
-    this.stack[0] = this.stateView.state.stack.last().root
+    this.stack[0] = this.state.stack.last().root
   }
 
   /**

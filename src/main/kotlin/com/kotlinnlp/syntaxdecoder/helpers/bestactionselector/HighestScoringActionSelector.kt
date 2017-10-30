a3bb3ba766389d15fd0e6a8ac374781d996ea06b
KotlinNLP Authors <github@kotlinnlp.com>
2017-10-30 02:21:09 +0100

    Add HighestScoringActionSelector

---
+++ src/main/kotlin/com/kotlinnlp/syntaxdecoder/helpers/bestactionselector/HighestScoringActionSelector.kt
@@ -0,0 +1,39 @@
+/* Copyright 2017-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package com.kotlinnlp.syntaxdecoder.helpers.bestactionselector
+
+import com.kotlinnlp.syntaxdecoder.DecodingContext
+import com.kotlinnlp.syntaxdecoder.items.StateItem
+import com.kotlinnlp.syntaxdecoder.transitionsystem.ExtendedState
+import com.kotlinnlp.syntaxdecoder.transitionsystem.Transition
+import com.kotlinnlp.syntaxdecoder.transitionsystem.state.State
+
+/**
+ * This [BestActionSelector] selects the first highest score action.
+ */
+class HighestScoringActionSelector<
+  StateType : State<StateType>,
+  TransitionType : Transition<TransitionType, StateType>,
+  ItemType : StateItem<ItemType, *, *>,
+  ContextType : DecodingContext<ContextType, ItemType>>
+  : BestActionSelector<StateType, TransitionType, ItemType, ContextType> {
+
+  /**
+   * @param actions a list of Actions sorted by descending order
+   * @param extendedState the extended state of the last scored actions
+   *
+   * @return the best action among the given [actions]
+   */
+  override fun select(
+    actions: List<Transition<TransitionType, StateType>.Action>,
+    extendedState: ExtendedState<StateType, TransitionType, ItemType, ContextType>
+  ): Transition<TransitionType, StateType>.Action {
+
+    return actions.first()
+  }
+}

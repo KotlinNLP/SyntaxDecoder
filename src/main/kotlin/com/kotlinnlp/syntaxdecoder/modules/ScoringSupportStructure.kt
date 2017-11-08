0ce93e7ea857d76324626f2fd74ca70c13091599
KotlinNLP Authors <github@kotlinnlp.com>
2017-11-08 17:37:24 +0100

    Remove generics and buildTransitionStructure() method

--- src/main/kotlin/com/kotlinnlp/syntaxdecoder/modules/ScoringSupportStructure.kt
+++ src/main/kotlin/com/kotlinnlp/syntaxdecoder/modules/ScoringSupportStructure.kt
@@ -7,35 +7,8 @@
 
 package com.kotlinnlp.syntaxdecoder.modules
 
-import com.kotlinnlp.syntaxdecoder.transitionsystem.Transition
-import com.kotlinnlp.syntaxdecoder.context.DecodingContext
-import com.kotlinnlp.syntaxdecoder.transitionsystem.state.ExtendedState
-import com.kotlinnlp.syntaxdecoder.transitionsystem.state.State
-import com.kotlinnlp.syntaxdecoder.context.items.StateItem
-import com.kotlinnlp.syntaxdecoder.modules.featuresextractor.features.Features
-
 /**
  * The support structure created for each scoring.
  * It contains data useful to extract features and score actions.
  */
-interface ScoringSupportStructure<
-  SelfType: ScoringSupportStructure<SelfType, StateType, TransitionType, ContextType, ItemType, FeaturesType>,
-  StateType : State<StateType>,
-  TransitionType : Transition<TransitionType, StateType>,
-  ContextType : DecodingContext<ContextType, ItemType>,
-  ItemType : StateItem<ItemType, *, *>,
-  FeaturesType : Features<*, *>> {
-
-  /**
-   * @param extendedState the extended state
-   * @param actions the actions to score
-   *
-   * @return a new transition support structure associated to this one
-   */
-  @Suppress("UNCHECKED_CAST")
-  fun buildTransitionStructure(
-    extendedState: ExtendedState<StateType, TransitionType, ItemType, ContextType>,
-    actions: List<Transition<TransitionType, StateType>.Action>
-  )
-    = TransitionSupportStructure(structure = this as SelfType, extendedState = extendedState, actions = actions)
-}
+interface ScoringSupportStructure

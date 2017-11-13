73c350f29c504e5ec098399f0a713340fc247e41
KotlinNLP Authors <github@kotlinnlp.com>
2017-11-13 14:58:13 +0100

    Update documentation

--- src/main/kotlin/com/kotlinnlp/syntaxdecoder/modules/actionserrorssetter/HingeLossActionsErrorsSetter.kt
+++ src/main/kotlin/com/kotlinnlp/syntaxdecoder/modules/actionserrorssetter/HingeLossActionsErrorsSetter.kt
@@ -18,7 +18,10 @@ import com.kotlinnlp.syntaxdecoder.transitionsystem.state.State
 /**
  * The [ActionsErrorsSetter] that implement the hinge-loss used for "maximum-margin" classification.
  *
- * @property learningMarginThreshold the margin below which to set the errors
+ * If the margin between the highest correct action score and the highest incorrect action score is greater then the
+ * given [learningMarginThreshold] then errors are relevant and they are set into the two actions mentioned above.
+ *
+ * @property learningMarginThreshold the learning margin threshold
  */
 class HingeLossActionsErrorsSetter<
   StateType : State<StateType>,

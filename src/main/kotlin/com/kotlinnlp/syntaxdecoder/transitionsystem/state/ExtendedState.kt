abe8aaea3b41b0783037fc6d1b2e1a3e4c27d040
KotlinNLP Authors <github@kotlinnlp.com>
2017-11-24 17:05:37 +0100

    Add scoreAccumulator parameter and replace score accumulation with its usage

--- src/main/kotlin/com/kotlinnlp/syntaxdecoder/transitionsystem/state/ExtendedState.kt
+++ src/main/kotlin/com/kotlinnlp/syntaxdecoder/transitionsystem/state/ExtendedState.kt
@@ -11,6 +11,7 @@ import com.kotlinnlp.syntaxdecoder.context.DecodingContext
 import com.kotlinnlp.syntaxdecoder.transitionsystem.oracle.Oracle
 import com.kotlinnlp.syntaxdecoder.context.items.StateItem
 import com.kotlinnlp.syntaxdecoder.transitionsystem.Transition
+import com.kotlinnlp.syntaxdecoder.transitionsystem.state.scoreaccumulator.ScoreAccumulator
 
 /**
  * The [ExtendedState] extends a [State] with the list of [StateItem]s that compose it and a [DecodingContext].
@@ -28,61 +29,39 @@ data class ExtendedState<
   ContextType : DecodingContext<ContextType, ItemType>>(
   val state: StateType,
   val context: ContextType,
+  val scoreAccumulator: ScoreAccumulator<*>,
   val oracle: Oracle<StateType, TransitionType>? = null) {
 
   /**
-   * The score of goodness of this state (a value in the range (0.0, 1.0]), default 1.0.
+   * The score of goodness of this state.
    */
-  val score: Double get() = 1 / (-0.1 * this.logScore + 1) // convert the domain (-inf, 0.0] to (0.0, 1.0]
+  val score: Double get() = this.scoreAccumulator.getCurrent()
 
   /**
-   * The score of goodness of this state (a value in the range (-inf, 0.0]), default 0.0.
-   * It is the result of more additions of the logarithm of scores in the range (0.0, 1.0], done calling the
-   * [accumulateScore] method.
-   */
-  var logScore: Double = 0.0
-    private set
-
-  /**
-   * The lower bound to limit accumulating scores (to avoid overflow and manage zero scores).
-   */
-  private val eps = 1.0e-08
-
-  /**
-   * Accumulate the given [score] into this state as joint probability of its score (after have transformed it by
-   * natural logarithm, to avoid underflow).
+   * Accumulate the given [addingScore] into this state .
    *
-   * @param score a score in the range [0.0, 1.0]
+   * @param addingScore the adding score
    */
-  fun accumulateScore(score: Double) {
-    require(score in 0.0 .. 1.0) { "Invalid score: $score, must be in range [0.0, 1.0]." }
-    this.logScore += Math.log(maxOf(score, this.eps))
-  }
+  fun accumulateScore(addingScore: Double) = this.scoreAccumulator.accumulate(addingScore)
 
   /**
    * Simulate the application of a given [action] to the [state], returning the estimated future score.
    *
    * @param action an action to apply to the [state]
    *
-   * @return the logScore that this state will assume if applying the given [action] to its [state]
+   * @return the score that this state will assume if applying the given [action] to its [state]
    */
   fun estimateFutureScore(action: Transition<TransitionType, StateType>.Action): Double
-    = this.logScore + Math.log(maxOf(action.score, this.eps))
+    = this.scoreAccumulator.estimateAccumulation(action.score)
 
   /**
    * @param state the new state that will replace the current one in the clone
    *
    * @return a clone of this [ExtendedState] replacing its state with the given [state]
    */
-  fun clone(state: StateType): ExtendedState<StateType, TransitionType, ItemType, ContextType> {
-
-    val clonedState = ExtendedState(
-      state = state,
-      context = this.context.copy(),
-      oracle = this.oracle?.copy())
-
-    clonedState.logScore = this.logScore
-
-    return clonedState
-  }
+  fun clone(state: StateType): ExtendedState<StateType, TransitionType, ItemType, ContextType> = ExtendedState(
+    state = state,
+    context = this.context.copy(),
+    oracle = this.oracle?.copy(),
+    scoreAccumulator = this.scoreAccumulator.copy())
 }

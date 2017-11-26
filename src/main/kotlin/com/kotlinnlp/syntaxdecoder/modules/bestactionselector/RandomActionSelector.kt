e30aaf1d27c6a848e37d4dbd20f7a7bfb323f375
KotlinNLP Authors <github@kotlinnlp.com>
2017-11-26 15:33:26 +0100

    Add RandomActionSelector

---
+++ src/main/kotlin/com/kotlinnlp/syntaxdecoder/modules/bestactionselector/RandomActionSelector.kt
@@ -0,0 +1,38 @@
+/* Copyright 2017-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package com.kotlinnlp.syntaxdecoder.modules.bestactionselector
+
+import com.kotlinnlp.syntaxdecoder.context.DecodingContext
+import com.kotlinnlp.syntaxdecoder.context.items.StateItem
+import com.kotlinnlp.syntaxdecoder.transitionsystem.state.ExtendedState
+import com.kotlinnlp.syntaxdecoder.transitionsystem.Transition
+import com.kotlinnlp.syntaxdecoder.transitionsystem.state.State
+import com.kotlinnlp.syntaxdecoder.utils.getRandom
+
+/**
+ * This [RandomActionSelector] randomly selects an action action among those allowed.
+ */
+class RandomActionSelector<
+  StateType : State<StateType>,
+  TransitionType : Transition<TransitionType, StateType>,
+  ItemType : StateItem<ItemType, *, *>,
+  ContextType : DecodingContext<ContextType, ItemType>>
+  : BestActionSelector<StateType, TransitionType, ItemType, ContextType> {
+
+  /**
+   * @param sortedActions a list of scored actions, sorted by descending score and then by transition priority
+   * @param extendedState the extended state of the last scored actions
+   *
+   * @return the best action among the given [sortedActions]
+   */
+  override fun select(
+    sortedActions: List<Transition<TransitionType, StateType>.Action>,
+    extendedState: ExtendedState<StateType, TransitionType, ItemType, ContextType>
+  ): Transition<TransitionType, StateType>.Action
+    = sortedActions.filter { it.isAllowed }.getRandom()
+}
